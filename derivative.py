b5ca8e8747dae3c0303a12bbf3b68e0a026582bc
lipilian <hongliuforever@gmail.com>
2020-03-13 23:01:46 -0500

    derivative

--- derivative.py
+++ derivative.py
@@ -9,4 +9,24 @@ import matplotlib.pyplot as plt
 x = torch.tensor(2., requires_grad=True)
 y = x**2
 y.backward()
+# %% complex example 
+u = torch.tensor(1.,requires_grad = True)
+v = torch.tensor(2.,requires_grad = True)
+f = u *v + u**2
+f.backward()
+print(u.grad)
+print(v.grad)
+# %% linspace get grad   
+x = torch.linspace(-10, 10, 1000, requires_grad = True)
+Y = torch.relu(x)
+y = Y.sum()
+y.backward()
+plt.plot(x.detach().numpy(), Y.detach().numpy(), label = 'function')
+plt.plot(x.detach().numpy(), x.grad.detach().numpy(), label = 'derivative')
+plt.xlabel('x')
+plt.legend()
+plt.show()
+print(y)
+
+
 # %%
